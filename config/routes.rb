42ce7928dd18d6ea0fa9a18aeff2b50f11d03930
EC2 Default User <ec2-user@ip-172-31-16-98.ec2.internal>
2022-03-14 04:57:18 +0000

    [FInish]books-debug-1

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get "homes/top" => "homes#top"
   resources :books, only: [:edit, :index, :show, :create, :update, :destroy]
+  patch 'books/:id' => 'books#update', as: 'update_book'
 end
